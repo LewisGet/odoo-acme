36a3214fc9fd424c9293c2943dae6611d0fccdc8
Lewis Jang <panjojo.com@gmail.com>
2024-03-01 19:58:03 +0800

    fixed file name have dash or bottom lines

--- controllers/regedit.py
+++ controllers/regedit.py
@@ -8,7 +8,7 @@ class LazyAcme(http.Controller):
 
     @http.route('/.well-known/acme-challenge/<string:file_name>', type='http', auth='public', methods=['GET'])
     def main(self, file_name, **get):
-        if not re.match("^[a-zA-Z0-9\\.]+$", file_name):
+        if not re.match("^[a-zA-Z0-9\\.\\-\\_]+$", file_name):
             return http.not_found()
 
         file_path = os.path.join(self.default_acme_path, file_name)
